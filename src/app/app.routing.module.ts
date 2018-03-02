b7860b63407d810085aec64d4fe0cde722a72f76
liang <zengliangi@163.com>
2018-03-02 09:51:27 +0800

    修改了route配置

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,15 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-
 const routes: Routes = [
     { path: '', redirectTo: '/dispatch', pathMatch: 'full' },
-    { path: 'dispatch', component: HomeComponent },
     { path: '**', component: HomeComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
